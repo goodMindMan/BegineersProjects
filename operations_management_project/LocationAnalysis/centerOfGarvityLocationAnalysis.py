386c4968f719b0ae5acef50aa09f2f482d96b061
poetman <125457215+goodMindMan@users.noreply.github.com>
2024-04-15 18:36:41 +0200

    Fix naming conventions centerOfGarvityLocationAnalysis.py

--- operations_management_project/LocationAnalysis/centerOfGarvityLocationAnalysis.py
+++ operations_management_project/LocationAnalysis/centerOfGarvityLocationAnalysis.py
@@ -2,14 +2,14 @@ def center_grav_location():
     import numpy as np
   
     # Total Number of Alternatives input
-    ValidAltNo = False # Initialize validity check variable
-    while not ValidAltNo: # Loop until input is valid
+    valid_alt_no = False # Initialize validity check variable
+    while not valid_alt_no: # Loop until input is valid
       try: 
-          AltNo  = int(input('How many Locations alternatives: --> ')) # Entering number of locations alternatives
+          alt_no  = int(input('How many Locations alternatives: --> ')) # Entering number of locations alternatives
           # Input validity check
-          if AltNo > 0: # Is it positive?
-              ValidAltNo = True  # Valid response
-              print(f'entry: {AltNo}') #Display Input
+          if alt_no > 0: # Is it positive?
+              valid_alt_no = True  # Valid response
+              print(f'entry: {alt_no}') #Display Input
           else:
               print('Enter a positive number')
       except ValueError:
@@ -18,71 +18,71 @@ def center_grav_location():
     # Creating a dictionary with the number of locations entered
     # X-Coords
     print('consider any place as the origin point and input vertical and horizontal distances')
-    Inc_Var_InX = 0 # Initialize counter
-    AltDictX = {} # Empty dictionary to be populated with X coordinates 
-    while Inc_Var_InX < AltNo:
+    inc_var_inx = 0 # Initialize counter
+    alt_dictx = {} # Empty dictionary to be populated with X coordinates 
+    while inc_var_inx < alt_no:
         try:
-            InX = float(input(f'Enter X (horizontal) coordinate of Location {Inc_Var_InX +1}: --> ')) # Input x coordinate
-            AltDictX.__setitem__(f'Location {Inc_Var_InX + 1} X coordinate', InX) # Populate empty Dict
-            Inc_Var_InX += 1 # Increment 
+            inx = float(input(f'Enter X (horizontal) coordinate of Location {inc_var_inx +1}: --> ')) # Input x coordinate
+            alt_dictx.__setitem__(f'Location {inc_var_inx + 1} X coordinate', inx) # Populate empty Dict
+            inc_var_inx += 1 # Increment 
         except ValueError:
             print('Enter a Whole or Decimal Number only')
-    print(AltDictX)
+    print(alt_dictx)
     # Y-coords
-    Inc_Var_InY = 0 # Initialize counter
-    AltDictY = {} # Empty dictionary to be populated with X coordinates 
-    while Inc_Var_InY < AltNo: # loop until Incremening __AltNo__ number of times
+    inc_var_iny = 0 # Initialize counter
+    alt_dicty = {} # Empty dictionary to be populated with X coordinates 
+    while inc_var_iny < alt_no: # loop until Incremening __alt_no__ number of times
         try:
-            InY = float(input(f'Enter y (vertical) coordinate of Location {Inc_Var_InY +1}: --> ')) # Input Y coordinate 
-            AltDictY.__setitem__(f'Location {Inc_Var_InY + 1} Y coordinate', InY) # Populate empty Dict
-            Inc_Var_InY += 1 # Increment
+            InY = float(input(f'Enter y (vertical) coordinate of Location {inc_var_iny +1}: --> ')) # Input Y coordinate 
+            alt_dicty.__setitem__(f'Location {inc_var_iny + 1} Y coordinate', InY) # Populate empty Dict
+            inc_var_iny += 1 # Increment
         except ValueError:
             print('Enter a Whole or Decimal Number only')
-    print(AltDictY)
+    print(alt_dicty)
 
     # Create an array of x and y coordinates
     # X-coords
-    Xcrds = np.array([]) # Empty array to be populated with coordinates for futhur processing
-    for i in AltDictX.values(): # populate array with all values of the dictionary
-        Xcrds = np.append(Xcrds, i)
+    xcrds = np.array([]) # Empty array to be populated with coordinates for futhur processing
+    for i in alt_dictx.values(): # populate array with all values of the dictionary
+        xcrds = np.append(xcrds, i)
     # Y-coords
-    Ycrds = np.array([]) # // for Y axis
-    for i in AltDictY.values(): # // for Y axis
-        Ycrds = np.append(Ycrds, i)
+    ycrds = np.array([]) # // for Y axis
+    for i in alt_dicty.values(): # // for Y axis
+        ycrds = np.append(ycrds, i)
 
     # weight or no weights
-    Exit = False # Initalize Loop Variables 
-    while Exit == False: # Loop only if there are weights or if all weights are entered
-        NoWhtsIn = input('Do alternatives have weights? y/n: --> ') # Prompt user 
+    exit_loop_var = False # Initalize Loop Variables 
+    while exit_loop_var == False: # Loop only if there are weights or if all weights are entered
+        no_whts_in = input('Do alternatives have weights? y/n: --> ') # Prompt user 
         #Input validity check
-        if NoWhtsIn in ['y', 'n']: 
-            if NoWhtsIn == 'y':
-                IsWhts = True
-                Exit = True
+        if no_whts_in in ['y', 'n']: 
+            if no_whts_in == 'y':
+                is_whts = True
+                exit_loop_var = True
             else:
-                IsWhts = False 
-                Exit = True # Assign it true to exit loop 
+                is_whts = False 
+                exit_loop_var = True # Assign it true to exit_loop_var loop 
         else:
             print('Please enter a valid response; No weights --> n, Has weights --> y') #
 
     # Input weights in array and calculating coordinates
-    if IsWhts == True:
+    if is_whts == True:
         Whts = np.array([]) # Empty array to be populated with Alternatives Weights if any
-        Inc_Var_Wht = 0 # Initialize counter
-        while Inc_Var_Wht < AltNo:
+        inc_var_wht = 0 # Initialize counter
+        while inc_var_wht < alt_no:
             try:
                 # Inputs
-                InWht = float(input(f'Please Enter Weights at the same order of Coordinates,,, weight {Inc_Var_Wht + 1}: --> ')) 
-                Whts = np.append(Whts, InWht) # Populate weights array
-                Inc_Var_Wht += 1 # increment
+                in_wht = float(input(f'Please Enter Weights at the same order of Coordinates,,, weight {inc_var_wht + 1}: --> ')) 
+                Whts = np.append(Whts, in_wht) # Populate weights array
+                inc_var_wht += 1 # increment
             except ValueError:
                 print('Please Enter a Whole or Decimal Number!')
         # Compute average weighted coordinates
-        AveX = np.average(Xcrds, None, Whts, 0)
-        AveY = np.average(Ycrds, None, Whts, 0)
-        print(f'center of gravity coordinates are ({AveX} , {AveY})')
+        avex = np.average(xcrds, None, Whts, 0)
+        avey = np.average(ycrds, None, Whts, 0)
+        print(f'center of gravity coordinates are ({avex} , {avey})')
     else:
         # Compute average coordinates  
-        AveX = np.average(Xcrds, None, None, 0)
-        AveY = np.average(Ycrds, None, None, 0)
-        print(f'center of gravity coordinates are ({AveX} , {AveY})')
+        avex = np.average(xcrds, None, None, 0)
+        avey = np.average(ycrds, None, None, 0)
+        print(f'center of gravity coordinates are ({avex} , {avey})')
